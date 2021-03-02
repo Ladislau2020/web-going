e7166533f75d71f824be4b8a0620bcfcd6ec2709
Ladislau André <ladislauandre32@gmail.com>
2021-03-02 21:15:39 +0100

    gits

---
+++ new.py
@@ -0,0 +1,28 @@
+Super Móvel
+Imagem
+Alimentos sofisticados para todos
+
+Produtos - Serviços -Política da empresa - Sobre nós - 
+imagens passando
+
+Seja cliente   -   Seja Colaborador  
+
+
+Contactos  - copyright 2021 - todos direitos reservados a Super Móvel Lda.  Termos e condições - Privacidade
+
+
+<wbr> para quebrar linhas
+<br> para quebrar parágrafos
+# &nbsp; dá espaço sem quebra. #
+&lt- <
+&gt- >
+&copy aparece sinal de direitos autorais
+&trade. 
+&sum - soma
+&infin - infinito
+&times - multiplicação
+&plusmn- mais ou menos
+&ne - diferente
+
+cria linha horizontal  <hr/>
+
